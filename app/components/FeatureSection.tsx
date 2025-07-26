050ada6987ef3fcc820e1c4c1141653612cde0df
anirudh-248 <anirudhphaniraj1@gmail.com>
2025-07-26 16:59:01 +0530

    fixed the route for the wardrobe screenshot

--- app/components/FeatureSection.tsx
+++ app/components/FeatureSection.tsx
@@ -66,7 +66,7 @@ export default function FeatureSection() {
           </div>
           <Image
             alt="Product screenshot"
-            src="https://storage.googleapis.com/vw-media-bucket/wardrobe-screenshot.png"
+            src="https://storage.googleapis.com/vw-media-bucket/wardrobe-ss.png"
             width={2432}
             height={1442}
             className="w-3xl max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-228 md:-ml-4 lg:-ml-0"
